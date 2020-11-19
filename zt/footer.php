7fb2d09656746802ae4ab71412d947ec8336d7d4
Cthulhux <Cthulhux@noemail.net>
2020-11-19 13:27:51 +0000

    Footer aufgeräumt
    FossilOrigin-Name: b13c96235c988462b4ccd87a51772e2ad28e3c20ed15a46dc973d1d20ba25f1c

--- zt/footer.php
+++ zt/footer.php
@@ -15,14 +15,8 @@
  */
  ?>
 
-<div id="footer"><form style="display:none" name="paypal" id="paypal" action="https://www.paypal.com/uk/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_xclick" />
-<input type="hidden" name="business" value="der_tuxman@arcor.de" />
-<input type="hidden" name="item_name" value="ZenTweet-Finanzierung" />
-<input type="hidden" name="currency_code" value="EUR" />
-<input type="hidden" name="amount" value="2.50" />
-</form>
-     ZenTweet. <a href="/zt/mission.txt" target="_blank">Mission Statement</a>. Idee: Internet. Inspiration: <a target="_blank" href="http://twitter.com/Fritten">@Fritten</a>. Umsetzung: <a target="_blank" href="http://twitter.com/tux0r">@tux0r</a>. Spenden: <a href="javascript:jQuery('#paypal').submit()">PayPal</a> oder <a href="https://flattr.com/profile/zentweet" target="_blank">Flattr</a>. Penis.</div>
+<div id="footer">
+     ZenTweet. <a href="/zt/mission.txt" target="_blank">Mission Statement</a>. Idee: Internet. Inspiration: <a target="_blank" href="http://twitter.com/Fritten">@Fritten</a>. Umsetzung: <a target="_blank" href="http://twitter.com/tux0r">@tux0r</a>. Spenden: <a href="https://paypal.me/GebtmireuerGeld">PayPal</a>. Penis.</div>
 
 </body>
 </html>
