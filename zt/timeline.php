ff83c2c0a1d3da19e28686b339c3b857bf8e49a3
Cthulhux <Cthulhux@noemail.net>
2020-12-21 12:09:33 +0000

    Deutschsprachige Kommentare - dann wenigstens überall.
    FossilOrigin-Name: 2e9f49889eaa6c37fa879422f214d1da7fd68b4cb2f2edaea7750bc6372028d6

--- zt/timeline.php
+++ zt/timeline.php
@@ -38,9 +38,9 @@ function fetch_tweets()
 
     $host = 'api.twitter.com';
     $method = 'GET';
-    $path = '/1.1/statuses/home_timeline.json'; // api call path
+    $path = '/1.1/statuses/home_timeline.json';
 
-    $query = array( // query parameters
+    $query = array(
         'screen_name' => $_SESSION["username"],
         'count' => '200'
     );
@@ -48,60 +48,52 @@ function fetch_tweets()
     $oauth = array(
         'oauth_consumer_key' => $consumer_key,
         'oauth_token' => $token,
-        'oauth_nonce' => (string)mt_rand(), // a stronger nonce is recommended
+        'oauth_nonce' => (string)mt_rand(),
         'oauth_timestamp' => time(),
         'oauth_signature_method' => 'HMAC-SHA1',
         'oauth_version' => '1.0'
     );
 
-    $oauth = array_map("rawurlencode", $oauth); // must be encoded before sorting
+    // Sortierung:
+    $oauth = array_map("rawurlencode", $oauth);
     $query = array_map("rawurlencode", $query);
 
-    $arr = array_merge($oauth, $query); // combine the values THEN sort
+    $arr = array_merge($oauth, $query);
 
-    asort($arr); // secondary sort (value)
-    ksort($arr); // primary sort (key)
+    asort($arr);
+    ksort($arr);
 
-    // http_build_query automatically encodes, but our parameters
-    // are already encoded, and must be by this point, so we undo
-    // the encoding step
+    // http_build_query encodiert das gesamte Query, aber unsere
+    // Anfrage enthält bereits encodierte Parameter - also einmal
+    // decodieren vorm Absenden:
     $querystring = urldecode(http_build_query($arr, '', '&'));
 
     $url = "https://$host$path";
-
-    // mash everything together for the text to hash
     $base_string = $method."&".rawurlencode($url)."&".rawurlencode($querystring);
-
-    // same with the key
     $key = rawurlencode($consumer_secret)."&".rawurlencode($token_secret);
 
-    // generate the hash
+    // Hash erzeugen:
     $signature = rawurlencode(base64_encode(hash_hmac('sha1', $base_string, $key, true)));
 
-    // this time we're using a normal GET query, and we're only encoding the query params
-    // (without the oauth params)
     $url .= "?".http_build_query($query);
     $url = str_replace("&amp;", "&", $url); //Patch by @Frewuill
 
-    $oauth['oauth_signature'] = $signature; // don't want to abandon all that work!
-    ksort($oauth); // probably not necessary, but twitter's demo does it
+    $oauth['oauth_signature'] = $signature;
+    ksort($oauth);
 
-    // also not necessary, but twitter's demo does this too
     $oauth = array_map("add_quotes", $oauth);
 
-    // this is the full value of the Authorization line
     $auth = "OAuth " . urldecode(http_build_query($oauth, '', ', '));
 
-    // if you're doing post, you need to skip the GET building above
-    // and instead supply query parameters to CURLOPT_POSTFIELDS
-    $options = array( CURLOPT_HTTPHEADER => array("Authorization: $auth"),
-    //CURLOPT_POSTFIELDS => $postfields,
-    CURLOPT_HEADER => false,
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_SSL_VERIFYPEER => false);
+    $options = array(
+        CURLOPT_HTTPHEADER => array("Authorization: $auth"),
+        CURLOPT_HEADER => false,
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false
+    );
 
-    // do our business
+    // Absenden und auswerten:
     $feed = curl_init();
     curl_setopt_array($feed, $options);
     $json = curl_exec($feed);
